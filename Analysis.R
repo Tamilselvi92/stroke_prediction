80490be76ac22cabb3d21fa5c43b9f282b3261b1
Tamil Selvi <tamilselvi922020@gmail.com>
2024-12-20 14:53:40 +0530

    a

--- Analysis.R
+++ Analysis.R
@@ -1,8 +1,9 @@
-print("Let us start our analysis")
 head(health_data)
 # Dimensions of the data
-dim(health_data)
 # any missing values
 sum(is.na(health_data))
+# to get the summary
 summary(health_data)
+# any missing values
+sum(is.na(health_data))
 
